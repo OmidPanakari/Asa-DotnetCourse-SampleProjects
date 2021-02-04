1d7fc4c7a500d82a7819e37e782dc88b4442203d
Omid <Asus@LAPTOP-QP6PFO6G>
2021-02-04 10:51:20 +0330

    New procedures

---
+++ Asa.ApartmentManagementSystem/Asa.ApartmentManagement.DB/dbo/Stored Procedures/expense_add.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE [dbo].[expense_add]
+	@category_id int,
+	@value decimal(5, 2),
+	@from datetime,
+	@to datetime,
+	@description nvarchar(50)
+AS
+	INSERT INTO [dbo].[Expenses](
+		[category_id],
+		[value],
+		[from],
+		[to],
+		[description]
+	)VALUES(
+		@category_id,
+		@value,
+		@from,
+		@to,
+		@description
+	)
+	SELECT SCOPE_IDENTITY()
+RETURN 0
